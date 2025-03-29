1ac40f10ab9d7df23b71ece11390a569a5aaf775
Sourabh dharra <s.dharra88@gamil.com>
2025-03-30 05:05:48 +0530

    url

--- Backend/index.js
+++ Backend/index.js
@@ -36,10 +36,10 @@ const app = express();
   main();
 
  
-  app.use("/api/v1/user",userRoute)
-  app.use("/api/v1/company",companyRoute)
-  app.use("/api/v1/job",jobRoute)
-  app.use("/api/v1/application",applicationRoute)
+  app.use("/user",userRoute)
+  app.use("/company",companyRoute)
+  app.use("/job",jobRoute)
+  app.use("/application",applicationRoute)
 
   // const PORT =8001
  const PORT =process.env.PORT;
