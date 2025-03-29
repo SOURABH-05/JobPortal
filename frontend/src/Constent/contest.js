62008b18ea75c090d51b2ed89590b82a3e9bd432
Sourabh dharra <s.dharra88@gamil.com>
2025-03-30 00:35:03 +0530

    add

--- frontend/src/Constent/contest.js
+++ frontend/src/Constent/contest.js
@@ -1,5 +1,5 @@
-export const USER_API_END_POINT="https://job-portal-two-phi.vercel.app/user";
-export const JOB_API_END_POINT="https://job-portal-two-phi.vercel.app/job";
-export const APPLICATION_API_END_POINT="https://job-portal-two-phi.vercel.app/application";
-export const COMPANY_API_END_POINT="https://job-portal-two-phi.vercel.app/company";
+export const USER_API_END_POINT="https://job-portal-zeta-self.vercel.app/user";
+export const JOB_API_END_POINT="https://job-portal-zeta-self.vercel.app/job";
+export const APPLICATION_API_END_POINT="https://job-portal-zeta-self.vercel.app/application";
+export const COMPANY_API_END_POINT="https://job-portal-zeta-self.vercel.app/company";
 
