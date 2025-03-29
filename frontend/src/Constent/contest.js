b9f5c665dd8526bdb6822f0a61fae13d2d36d7f8
Sourabh dharra <s.dharra88@gamil.com>
2025-03-30 05:19:14 +0530

    done

--- frontend/src/Constent/contest.js
+++ frontend/src/Constent/contest.js
@@ -1,5 +1,5 @@
-export const USER_API_END_POINT="https://jobportal-hpta.onrender.com/user";
-export const JOB_API_END_POINT="https://jobportal-hpta.onrender.com/job";
-export const APPLICATION_API_END_POINT="https://jobportal-hpta.onrender.com/application";
-export const COMPANY_API_END_POINT="https://jobportal-hpta.onrender.com/company";
+export const USER_API_END_POINT="https://jobportal-hpta.onrender.com/api/v1/user";
+export const JOB_API_END_POINT="https://jobportal-hpta.onrender.com/api/v1/job";
+export const APPLICATION_API_END_POINT="https://jobportal-hpta.onrender.com/api/v1/application";
+export const COMPANY_API_END_POINT="https://jobportal-hpta.onrender.com/api/v1/company";
 
